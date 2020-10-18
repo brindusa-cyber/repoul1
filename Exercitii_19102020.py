d7bbdc1fad746b6772a94da2e4dda89e9c49ed66
brindusa-cyber <brandusa_simionescu@yahoo.com>
2020-10-18 23:53:28 +0100

    cod din 12.10.2020

---
+++ Exercitii_19102020.py
@@ -0,0 +1,50 @@
+#Exercitiul 1(a)
+# Exercitiul 1(a)# se repeta pt fiecare caracter din sir, trebuie sa-l mai gandesc
+# sir_user = input('Introduceti sir:')
+# a = 0
+# for item in sir_user:
+#     print("Sirul introdus are", len(sir_user), "caractere")
+#######
+#Exercitiul 1(b)
+# sir_user = input('Introduceti sir:')
+# a = 0
+# for item in sir_user:
+#     a += 1
+#     print("Sirul introdus are", a, "caractere")
+#########################
+# import random
+# lista_cuvinte = ["palarie", "deget", "picior", "abecedar"]
+# cuv_ales = random.choice(lista_cuvinte)
+# litere_lipsa = len(cuv_ales)-2
+# display = cuv_ales.replace(cuv_ales[1:-1],"-"*(litere_lipsa))
+# print ("Cuvantul are", len(cuv_ales), "litere",":")
+# print(display)
+# lit = input(str("Introdu o litera:")),
+# d=list(cuv_ales)
+# # pozitie=
+# count = 7
+# while count< 7:
+#     count = count-1
+# # cuv_ales.find(lit[, start[, end]])
+# print("Mai aveti ",count, "incercari"),
+# # if lit in cuv_ales:
+# print(display)
+###################################
+#Inca nu am reusit, imi mai trebuie timp
+nr_telefon = input(str("Scrieti numarul de telefon:"))
+lungime = len(nr_telefon),
+pos1 = nr_telefon.find("@")
+pos2 = nr_telefon.find(".")
+if (nr_telefon[0])!="0":
+    print("Nr de telefon trebuie sa inceapa cu 0")
+elif (nr_telefon[0]) == "0":
+    pass
+elif len(nr_telefon)>10 or len(nr_telefon) < 10:
+print("Nr de telefon trebuie sa aiba 10 cifre")
+if "@" in nr_telefon = True and "." in nr_telefon = True and pos1 < pos2:
+    elif pos1 > pos2
+    print ("Nr de tel este incorect, mai verificati")
+    elif
+# trebuie sa adaug inca o conditie pentru ca  @ si . sa se regaseasca o sg data in nr., dar e prea tarziu
+else print("Nr. de telefon => valid")
+#####################################
